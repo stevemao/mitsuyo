4d88527c7aadc69e02b5724300a6b35a5f37757b
Steve Mao <maochenyan@gmail.com>
2017-02-18 15:56:45 +1100

    init

---
+++ app.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+from future import standard_library
+standard_library.install_aliases()
+import urllib.request, urllib.parse, urllib.error
+import json
+import os
+
+from flask import Flask
+from flask import request
+from flask import make_response
+from chatterbot import ChatBot
+
+# Flask app should start in global layout
+app = Flask(__name__)
+
+chatbot = ChatBot(
+    'Steve Mao',
+    trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
+)
+
+# Train based on the japanese corpus
+chatbot.train("chatterbot.corpus.japanese")
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = processRequest(req)
+
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+def processRequest(req):
+    speech = chatbot.get_response(req.get('result').get('resolvedQuery'))
+    speech = str(speech)
+
+    print("Response:")
+    print(speech)
+
+    return {
+        "speech": speech,
+        "displayText": speech,
+        # "data": data,
+        # "contextOut": [],
+        "source": "ichat-us"
+    }
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5000))
+
+    print("Starting app on port %d" % port)
+
+    app.run(debug=False, port=port, host='0.0.0.0')
